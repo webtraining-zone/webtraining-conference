de4117108e00faa802323267da0a01715bf0e4e2
Alex Arriaga <alexzander.arriaga@base22.com>
2018-08-01 18:17:23 -0500

    Agregar relaciones entre modelos Talk y User

---
+++ database/migrations/2018_08_01_224520_create_talks_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTalksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('talks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('summary');
+            $table->unsignedInteger('user_id');
+            $table->string('thumbnail')->nullable();
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->dateTime('starts_at');
+            $table->dateTime('ends_at');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('talks');
+    }
+}
