aedbcac1e4cf7d54e32b03c1afa46eefa8e701dc
Alex Arriaga <alexzander.arriaga@base22.com>
2018-08-01 18:17:58 -0500

    Mejorar estilos en agenda

--- resources/views/program/schedule.blade.php
+++ resources/views/program/schedule.blade.php
@@ -7,35 +7,29 @@
                 </h2>
 
                 <h3>Tuesday</h3>
-
-                <div class="b-schedule-item">
-                    <div class="row">
-                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                            <div class="b-schedule-item__date">
-                                <span class="b-schedule-item__time">
-                                    09:00 - 11:00
-                                </span>
-                            </div>
-                        </div>
-                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                            <a href="">
-                                <h4 class="b-schedule-item__title">
-                                    Progressive Web Apps with React
-                                </h4>
-                            </a>
-
-                            <span class="b-schedule-item__place text-uppercase">Room C</span>
-                            <p class="b-schedule-item__summary">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur eum eveniet
-                                laborum laudantium suscipit. Architecto cum dolores itaque quas quasi rem tempora. Culpa
-                                eius id ipsam pariatur quos rem veniam!
-                            </p>
-
-                        </div>
+                <article class="b-schedule-item">
+                    <div class="b-schedule-item-header__date">
+                        <span class="b-schedule-item-header__time">
+                                        09:00 - 11:00
+                        </span>
                     </div>
 
+                    <div class="b-schedule-item-body">
+                        <a href="">
+                            <h4 class="b-schedule-item__title">
+                                Progressive Web Apps with React
+                            </h4>
+                        </a>
+                        <span class="b-schedule-item__place text-uppercase">Room C</span>
+                        <p class="b-schedule-item__summary">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur eum eveniet
+                            laborum laudantium suscipit. Architecto cum dolores itaque quas quasi rem tempora. Culpa
+                            eius id ipsam pariatur quos rem veniam!
+                        </p>
+                    </div>
+                </article>
 
-                </div>
+
             </div>
         </div>
     </div>
