c93c8c46cb91d9c33b8f8e9bdae959da7e15426e
Alex Arriaga <alexzander.arriaga@base22.com>
2018-08-02 18:29:08 -0500

    Modificar controlador de programa para obtener las charlas

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Talk;
 use Illuminate\Http\Request;
 
 class ProgramController extends Controller
 {
     public function index()
     {
-        return view('program.index');
+        $talks = Talk::all();
+        return view('program.index')->with('talks', $talks);
     }
 }
