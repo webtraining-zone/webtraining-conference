e90b316f00ddad840f0ad0ada0113602325f9f1d
Alex Arriaga <alexzander.arriaga@base22.com>
2018-08-02 18:27:59 -0500

    Agregar modelos y sus relaciones

--- app/Talk.php
+++ app/Talk.php
@@ -8,6 +8,11 @@ class Talk extends Model
 {
     public function speaker()
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function room()
+    {
+        return $this->belongsTo('App\Room');
     }
 }
