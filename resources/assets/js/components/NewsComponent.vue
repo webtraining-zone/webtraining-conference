1e3a5418ff5912fe2718a5f4f5ed6ebb3d5355e8
Alex Arriaga <alexzander.arriaga@base22.com>
2018-08-03 15:24:13 -0500

    Crear componente de view para reemplazar el ejemplo estático

---
+++ resources/assets/js/components/NewsComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="b-news-component">
+        <article class="b-news" v-for="news in newsItems">
+            <div class="row">
+                <div class="col-4 col-sm-2 col-md-2 col-lg-1 col-xl-1">
+                    <div class="b-news__date">
+                                <span class="b-news__day">
+                                    {{ news.published_at | formatDate('DD') }}
+                                </span>
+                        <span class="b-news__month">
+                                    {{ news.published_at | formatDate('MMM') }}
+                                </span>
+                        <span class="b-news__year">
+                                    {{ news.published_at | formatDate('YYYY') }}
+                                </span>
+                    </div>
+                </div>
+                <div class="col-8 col-sm-10 col-md-10 col-lg-11 col-xl-11">
+
+                    <a href="#">
+                        <h3 class="b-news__title">
+                            {{ news.title }}
+                        </h3>
+                    </a>
+                    <span class="b-news__place text-uppercase">{{ news.place }}</span>
+                    <p class="b-news__summary">
+                        {{ news.summary }}
+                    </p>
+
+                </div>
+            </div>
+        </article>
+    </section>
+</template>
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'news-component',
+        data() {
+            return {
+                newsItems: []
+            };
+        },
+        created() {
+            axios.get('/api/news').then(response => {
+                this.newsItems = response.data;
+            });
+        }
+    }
+</script>
--- resources/views/welcome/news-and-updates.blade.php
+++ resources/views/welcome/news-and-updates.blade.php
@@ -6,40 +6,8 @@
                     Noticias y avisos
                 </h2>
 
-                <div class="b-news">
-                    <div class="row">
-                        <div class="col-4 col-sm-2 col-md-2 col-lg-1 col-xl-1">
-                            <div class="b-news__date">
-                                <span class="b-news__day">
-                                    27
-                                </span>
-                                <span class="b-news__month">
-                                    Jul
-                                </span>
-                                <span class="b-news__year">
-                                    2018
-                                </span>
-                            </div>
-                        </div>
-                        <div class="col-8 col-sm-10 col-md-10 col-lg-11 col-xl-11">
-                            {{--<div class="b-news__extra-information">--}}
-                            <a href="#">
-                                <h3 class="b-news__title">
-                                    Ya está confirmada la fecha y lugar de la primera edición de Webtraining Conference
-                                </h3>
-                            </a>
-                            <span class="b-news__place text-uppercase">Monterrey, NL, México 2018</span>
-                            <p class="b-news__summary">
-                                Prepara tus maletas, ya tenemos la sede lista para la primera edición del evento sobre
-                                la web más genial del año. Ven y aprende de muchos profesionales de la web reunidos en
-                                un mismo lugar. Sé parte de esta experiencia y aprende cómo se hacen proyectos en las grandes ligas.
-                            </p>
-                            {{--</div>--}}
-                        </div>
-                    </div>
+                <news-component></news-component>
 
-
-                </div>
             </div>
         </div>
     </div>
