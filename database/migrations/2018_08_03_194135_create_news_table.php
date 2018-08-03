f8ae4a2885274a71e266f0681b0353c67ae6b91d
Alex Arriaga <alexzander.arriaga@base22.com>
2018-08-03 15:23:45 -0500

    Agregar migración para noticias

---
+++ database/migrations/2018_08_03_194135_create_news_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('summary');
+            $table->text('body');
+            $table->unsignedInteger('user_id');
+            $table->string('thumbnail')->nullable();
+            $table->string('image')->nullable();
+            $table->dateTime('published_at');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
