7a6cfdbd475bf154e0780c7332a72d327adf2a10
Alex Arriaga <alexzander.arriaga@base22.com>
2018-08-06 16:43:24 -0500

    Agregar ejemplo de grouping

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Talk;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ProgramController extends Controller
 {
     public function index()
     {
+//        Carbon::setLocale('es_ES');
+//        setlocale(LC_TIME, "es_ES.UTF-8");
         $talks = Talk::all();
-        return view('program.index')->with('talks', $talks);
+
+        $groups = $talks->map(function ($talk, $key) {
+            $talk['day'] = Carbon::parse($talk->starts_at)->format('l');
+            return $talk;
+        })->groupBy('day');
+
+        return view('program.index')->with('groups', $groups);
     }
 
     public function showTalk(Talk $talk)
--- resources/views/program/schedule.blade.php
+++ resources/views/program/schedule.blade.php
@@ -6,47 +6,47 @@
                     Agenda
                 </h2>
 
-                <h3>Tuesday</h3>
-                @foreach($talks as $talk)
-                    <article class="b-schedule-item">
+                @foreach($groups as $key => $talks)
+                    <h3>{{ $key }}</h3>
+                    @foreach($talks as $talk)
+                        <article class="b-schedule-item">
 
-                        <div class="b-schedule-item-header__date">
+                            <div class="b-schedule-item-header__date">
                             <span class="b-schedule-item-header__time">
                                 {{ Carbon\Carbon::parse($talk->starts_at)->format('H:i') }}
                                 -
                                 {{ Carbon\Carbon::parse($talk->ends_at)->format('H:i') }}
                             </span>
-                        </div>
+                            </div>
 
-                        <div class="b-schedule-item-body">
-                            <div class="row">
-                                <div class="col-12 col-sm-7 col-md-5 col-lg-4 col-xl-4">
-                                    <img src="{{ $talk->thumbnail }}" alt="{{ $talk->speaker->name }}"
-                                         class="img-fluid rounded">
-                                </div>
-                                <div class="col-12 col-sm-7 col-md-7 col-lg-8 col-xl-8">
-                                    <span class="b-schedule-item__place text-uppercase">{{ $talk->room->name }}</span>
-                                    <a href="{{ route('showTalk', $talk->slug) }}">
-                                        <h4 class="b-schedule-item__title">
-                                            {{ $talk->title }}
-                                        </h4>
-                                    </a>
+                            <div class="b-schedule-item-body">
+                                <div class="row">
+                                    <div class="col-12 col-sm-7 col-md-5 col-lg-4 col-xl-4">
+                                        <img src="{{ $talk->thumbnail }}" alt="{{ $talk->speaker->name }}"
+                                             class="img-fluid rounded">
+                                    </div>
+                                    <div class="col-12 col-sm-7 col-md-7 col-lg-8 col-xl-8">
+                                        <span class="b-schedule-item__place text-uppercase">{{ $talk->room->name }}</span>
+                                        <a href="{{ route('showTalk', $talk->slug) }}">
+                                            <h4 class="b-schedule-item__title">
+                                                {{ $talk->title }}
+                                            </h4>
+                                        </a>
 
-                                    <p>
-                                        <strong>Conferencista: </strong>
-                                        <span class="b-schedule-item__speaker">{{ $talk->speaker->name }}</span>
-                                    </p>
+                                        <p>
+                                            <strong>Conferencista: </strong>
+                                            <span class="b-schedule-item__speaker">{{ $talk->speaker->name }}</span>
+                                        </p>
 
-                                    <p class="b-schedule-item__summary">
-                                        {!!  $talk->summary  !!}
-                                    </p>
+                                        <p class="b-schedule-item__summary">
+                                            {!!  $talk->summary  !!}
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </article>
+                        </article>
+                    @endforeach
                 @endforeach
-
-
             </div>
         </div>
     </div>
